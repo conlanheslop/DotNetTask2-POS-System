b376d14500cee9289e329427a6e61ae44b1fa85e
Ali B <30826345+PrinceAlii@users.noreply.github.com>
2024-10-28 01:07:42 +1100

    New UI courtesy of Ian

--- POS System/Forms/LoginScreen.Designer.cs
+++ POS System/Forms/LoginScreen.Designer.cs
@@ -30,127 +30,142 @@
         {
             components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginScreen));
-            label2 = new Label();
-            PasswordTextBox = new MaskedTextBox();
-            LoginButton = new Button();
-            label3 = new Label();
-            label4 = new Label();
-            timer1 = new System.Windows.Forms.Timer(components);
-            label1 = new Label();
-            UsernameTextBox = new TextBox();
             tableLayoutPanel1 = new TableLayoutPanel();
+            label1 = new Label();
+            LoginButton = new Button();
+            label4 = new Label();
+            PasswordTextBox = new MaskedTextBox();
+            UsernameTextBox = new TextBox();
+            label3 = new Label();
+            label2 = new Label();
+            label5 = new Label();
+            timer1 = new System.Windows.Forms.Timer(components);
             tableLayoutPanel1.SuspendLayout();
             SuspendLayout();
             // 
-            // label2
+            // tableLayoutPanel1
             // 
-            label2.Anchor = AnchorStyles.Top;
-            label2.AutoSize = true;
-            label2.Font = new Font("Segoe UI Semibold", 18F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
-            label2.ImageAlign = ContentAlignment.MiddleRight;
-            label2.Location = new Point(299, 51);
-            label2.Margin = new Padding(2, 0, 2, 0);
-            label2.Name = "label2";
-            label2.Size = new Size(74, 32);
-            label2.TabIndex = 1;
-            label2.Text = "Login";
-            // 
-            // PasswordTextBox
-            // 
-            PasswordTextBox.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
-            PasswordTextBox.Location = new Point(197, 273);
-            PasswordTextBox.Margin = new Padding(2);
-            PasswordTextBox.Name = "PasswordTextBox";
-            PasswordTextBox.PasswordChar = '*';
-            PasswordTextBox.Size = new Size(279, 23);
-            PasswordTextBox.TabIndex = 2;
-            // 
-            // LoginButton
-            // 
-            LoginButton.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            LoginButton.Location = new Point(197, 333);
-            LoginButton.Margin = new Padding(2);
-            LoginButton.Name = "LoginButton";
-            LoginButton.Size = new Size(279, 48);
-            LoginButton.TabIndex = 4;
-            LoginButton.Text = "Login";
-            LoginButton.UseVisualStyleBackColor = true;
-            LoginButton.Click += LoginButton_Click;
-            // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
-            label3.Location = new Point(197, 135);
-            label3.Margin = new Padding(2, 0, 2, 0);
-            label3.Name = "label3";
-            label3.Size = new Size(87, 21);
-            label3.TabIndex = 5;
-            label3.Text = "Username";
-            label3.Click += label3_Click;
-            // 
-            // label4
-            // 
-            label4.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
-            label4.AutoSize = true;
-            label4.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
-            label4.Location = new Point(197, 244);
-            label4.Margin = new Padding(2, 0, 2, 0);
-            label4.Name = "label4";
-            label4.Size = new Size(279, 21);
-            label4.TabIndex = 6;
-            label4.Text = "Password";
+            tableLayoutPanel1.ColumnCount = 1;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.Controls.Add(label1, 0, 1);
+            tableLayoutPanel1.Controls.Add(LoginButton, 0, 9);
+            tableLayoutPanel1.Controls.Add(label4, 0, 6);
+            tableLayoutPanel1.Controls.Add(PasswordTextBox, 0, 7);
+            tableLayoutPanel1.Controls.Add(UsernameTextBox, 0, 5);
+            tableLayoutPanel1.Controls.Add(label3, 0, 4);
+            tableLayoutPanel1.Controls.Add(label2, 0, 3);
+            tableLayoutPanel1.Controls.Add(label5, 0, 2);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Margin = new Padding(2);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 11;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 13F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 33F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 31F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 49F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 40F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 67F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 40F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 67F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 20F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 80F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 13F));
+            tableLayoutPanel1.Size = new Size(666, 507);
+            tableLayoutPanel1.TabIndex = 8;
             // 
             // label1
             // 
             label1.Anchor = AnchorStyles.Top;
-            label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI", 16F, FontStyle.Regular, GraphicsUnit.Point);
-            label1.Location = new Point(232, 0);
+            label1.Font = new Font("Segoe UI", 16F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(223, 13);
             label1.Margin = new Padding(2, 0, 2, 0);
             label1.Name = "label1";
-            label1.Size = new Size(209, 30);
+            label1.Size = new Size(220, 30);
             label1.TabIndex = 0;
             label1.Text = "DotNET POS System";
-            label1.Click += label1_Click;
+            // 
+            // LoginButton
+            // 
+            LoginButton.Anchor = AnchorStyles.Top;
+            LoginButton.BackColor = Color.RoyalBlue;
+            LoginButton.Cursor = Cursors.Hand;
+            LoginButton.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            LoginButton.ForeColor = SystemColors.HighlightText;
+            LoginButton.Location = new Point(238, 372);
+            LoginButton.Margin = new Padding(2);
+            LoginButton.Name = "LoginButton";
+            LoginButton.Size = new Size(190, 47);
+            LoginButton.TabIndex = 4;
+            LoginButton.Text = "Log In";
+            LoginButton.UseVisualStyleBackColor = false;
+            // 
+            // label4
+            // 
+            label4.Anchor = AnchorStyles.None;
+            label4.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.Location = new Point(222, 242);
+            label4.Margin = new Padding(2, 0, 2, 0);
+            label4.Name = "label4";
+            label4.Size = new Size(221, 21);
+            label4.TabIndex = 6;
+            label4.Text = "Password";
+            // 
+            // PasswordTextBox
+            // 
+            PasswordTextBox.Anchor = AnchorStyles.Top;
+            PasswordTextBox.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            PasswordTextBox.Location = new Point(221, 275);
+            PasswordTextBox.Margin = new Padding(2);
+            PasswordTextBox.Name = "PasswordTextBox";
+            PasswordTextBox.PasswordChar = '*';
+            PasswordTextBox.Size = new Size(223, 25);
+            PasswordTextBox.TabIndex = 2;
             // 
             // UsernameTextBox
             // 
-            UsernameTextBox.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            UsernameTextBox.Anchor = AnchorStyles.Top;
             UsernameTextBox.Cursor = Cursors.IBeam;
-            UsernameTextBox.Location = new Point(197, 166);
+            UsernameTextBox.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            UsernameTextBox.Location = new Point(221, 168);
             UsernameTextBox.Margin = new Padding(2);
             UsernameTextBox.Name = "UsernameTextBox";
-            UsernameTextBox.Size = new Size(279, 23);
+            UsernameTextBox.Size = new Size(223, 25);
             UsernameTextBox.TabIndex = 1;
             // 
-            // tableLayoutPanel1
+            // label3
             // 
-            tableLayoutPanel1.ColumnCount = 3;
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 40.79498F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 59.20502F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 187F));
-            tableLayoutPanel1.Controls.Add(label1, 1, 0);
-            tableLayoutPanel1.Controls.Add(label2, 1, 1);
-            tableLayoutPanel1.Controls.Add(label3, 1, 2);
-            tableLayoutPanel1.Controls.Add(label4, 1, 4);
-            tableLayoutPanel1.Controls.Add(UsernameTextBox, 1, 3);
-            tableLayoutPanel1.Controls.Add(PasswordTextBox, 1, 5);
-            tableLayoutPanel1.Controls.Add(LoginButton, 1, 6);
-            tableLayoutPanel1.Dock = DockStyle.Fill;
-            tableLayoutPanel1.Location = new Point(0, 0);
-            tableLayoutPanel1.Name = "tableLayoutPanel1";
-            tableLayoutPanel1.RowCount = 8;
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 37.77778F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 62.22222F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 29F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 80F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 27F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 60F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 52F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 123F));
-            tableLayoutPanel1.Size = new Size(666, 507);
-            tableLayoutPanel1.TabIndex = 7;
+            label3.Anchor = AnchorStyles.None;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(222, 135);
+            label3.Margin = new Padding(2, 0, 2, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(222, 21);
+            label3.TabIndex = 5;
+            label3.Text = "Username";
+            // 
+            // label2
+            // 
+            label2.Anchor = AnchorStyles.Bottom;
+            label2.Font = new Font("Segoe UI Semibold", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.ImageAlign = ContentAlignment.MiddleRight;
+            label2.Location = new Point(221, 90);
+            label2.Margin = new Padding(2, 0, 2, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(223, 36);
+            label2.TabIndex = 1;
+            label2.Text = "Login";
+            // 
+            // label5
+            // 
+            label5.Anchor = AnchorStyles.Top;
+            label5.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.Location = new Point(252, 46);
+            label5.Margin = new Padding(2, 0, 2, 0);
+            label5.Name = "label5";
+            label5.Size = new Size(162, 25);
+            label5.TabIndex = 7;
+            label5.Text = "Point of sale system";
             // 
             // LoginScreen
             // 
@@ -172,14 +187,16 @@
         }
 
         #endregion
-        private Label label2;
-        private MaskedTextBox PasswordTextBox;
-        private Button LoginButton;
-        private Label label3;
-        private Label label4;
-        private System.Windows.Forms.Timer timer1;
-        private Label label1;
-        private TextBox UsernameTextBox;
+
         private TableLayoutPanel tableLayoutPanel1;
+        private Label label1;
+        private Button LoginButton;
+        private Label label4;
+        private MaskedTextBox PasswordTextBox;
+        private TextBox UsernameTextBox;
+        private Label label3;
+        private Label label2;
+        private Label label5;
+        private System.Windows.Forms.Timer timer1;
     }
 }
